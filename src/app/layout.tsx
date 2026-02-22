0b009ca8631b43fcbf279f42f75441a223c714b4
Forfold <nathanieljcook@outlook.com>
2026-02-21 20:49:10 -0800

    fix theme bug on reload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <AppRouterCacheProvider>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <Providers>
             <AuthGate>
               {children}
