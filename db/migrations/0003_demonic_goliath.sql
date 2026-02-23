5dcfe42bfe4e96aae4ea179a3df4ab5a9145066b
Forfold <nathanieljcook@outlook.com>
2026-02-22 17:30:11 -0800

    fix migrations

--- db/migrations/0003_demonic_goliath.sql
+++ db/migrations/0003_demonic_goliath.sql
@@ -1,3 +1,2 @@
 ALTER TABLE "accounts" ADD COLUMN "refresh_token" text;--> statement-breakpoint
-ALTER TABLE "accounts" ADD COLUMN "session_state" text;--> statement-breakpoint
-ALTER TABLE "form_submissions" ADD COLUMN "pdf_storage_key" text;
+ALTER TABLE "accounts" ADD COLUMN "session_state" text;
