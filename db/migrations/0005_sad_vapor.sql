6f56634568a44ae4395be30bbcef5b3143793c83
Forfold <nathanieljcook@outlook.com>
2026-02-24 22:00:33 -0800

    npm run db:generate

---
+++ db/migrations/0005_sad_vapor.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "form_submissions" ADD COLUMN "facility_name" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "form_submissions" ADD COLUMN "facility_address" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "form_submissions" ADD COLUMN "permit_number" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "form_submissions" ADD COLUMN "inspection_date" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "form_submissions" ADD COLUMN "inspector_name" text NOT NULL;
