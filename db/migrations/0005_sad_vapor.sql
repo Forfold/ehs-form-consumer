46b2728c03fa488302d8e461e1d84b53e73d778d
Forfold <nathanieljcook@outlook.com>
2026-02-24 22:14:00 -0800

    fix migration

--- db/migrations/0005_sad_vapor.sql
+++ db/migrations/0005_sad_vapor.sql
@@ -1,5 +1,19 @@
-ALTER TABLE "form_submissions" ADD COLUMN "facility_name" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "form_submissions" ADD COLUMN "facility_address" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "form_submissions" ADD COLUMN "permit_number" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "form_submissions" ADD COLUMN "inspection_date" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "form_submissions" ADD COLUMN "inspector_name" text NOT NULL;
+ALTER TABLE "form_submissions" ADD COLUMN IF NOT EXISTS "facility_name" text NOT NULL DEFAULT '';
+--> statement-breakpoint
+ALTER TABLE "form_submissions" ADD COLUMN IF NOT EXISTS "facility_address" text NOT NULL DEFAULT '';
+--> statement-breakpoint
+ALTER TABLE "form_submissions" ADD COLUMN IF NOT EXISTS "permit_number" text NOT NULL DEFAULT '';
+--> statement-breakpoint
+ALTER TABLE "form_submissions" ADD COLUMN IF NOT EXISTS "inspection_date" text NOT NULL DEFAULT '';
+--> statement-breakpoint
+ALTER TABLE "form_submissions" ADD COLUMN IF NOT EXISTS "inspector_name" text NOT NULL DEFAULT '';
+--> statement-breakpoint
+ALTER TABLE "form_submissions" ALTER COLUMN "facility_name"    DROP DEFAULT;
+--> statement-breakpoint
+ALTER TABLE "form_submissions" ALTER COLUMN "facility_address" DROP DEFAULT;
+--> statement-breakpoint
+ALTER TABLE "form_submissions" ALTER COLUMN "permit_number"    DROP DEFAULT;
+--> statement-breakpoint
+ALTER TABLE "form_submissions" ALTER COLUMN "inspection_date"  DROP DEFAULT;
+--> statement-breakpoint
+ALTER TABLE "form_submissions" ALTER COLUMN "inspector_name"   DROP DEFAULT;
